2e5bfc2faa1e0e70e313145698f20d83a4d872f9
Hugo Lindström <hugo.lindstrom@trimble.se>
2019-06-19 15:10:24 +0200

    Add CMake

---
+++ cmake/CMakeModulePaths.cmake
@@ -0,0 +1,18 @@
+#Make sure that custom modules are found
+
+set(MODULE_PATH_DIR "${CMAKE_CURRENT_LIST_DIR}/../../../cmake")
+
+if(NOT "${MODULE_PATH_DIR}" IN_LIST CMAKE_MODULE_PATH)
+    list(APPEND CMAKE_MODULE_PATH "${MODULE_PATH_DIR}")
+endif()
+if(NOT "${MODULE_PATH_DIR}/cmake/modules" IN_LIST CMAKE_MODULE_PATH)
+    list(APPEND CMAKE_MODULE_PATH "${MODULE_PATH_DIR}/modules")
+endif()
+
+if(EXISTS "${MODULE_PATH_DIR}/DefaultCMakeBuildSettings.cmake")
+    message(STATUS "Found AppDev CMake Modules")
+    set(APPDEV_CMAKE_FOUND TRUE)
+else()
+    message(FATAL_ERROR "Did not find AppDev CMake Modules in ${CMAKE_MODULE_PATH}")
+endif()
+
